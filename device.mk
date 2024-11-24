07bc8b30cb8b5d32864993c2d5195404bf32bf71
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-11-24 09:38:20 +0300

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/alps/ac8257_demo
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
